35e62d07ac062b012619cc3f51651853b64a8d54
floarlesss <117862164+floarlesss@users.noreply.github.com>
2023-01-08 22:38:09 +0000

    add gamesreader

--- modules/gamesreader.py
+++ modules/gamesreader.py
@@ -3,12 +3,39 @@ import os
 working_dir = os.getcwd()
 
 def getNumberOfGames():
-    arcademenudir = working_dir[8:]
-    gameslistdir = arcademenudir + "/gameslist/games.txt"
+    global number_of_games
+    number_of_games = 0
 
+    arcademenudir = working_dir[:-8]
+    gameslistdir = arcademenudir + "\\gameslist\\games.txt"
+
+    # Open the games list .txt file
     with open(gameslistdir,"r") as f:
+        # Loop through all the lines in the file
         for line in f:
-            #do something here
+            global newgame, ln
+            # If the line starts with "#", pass (because it's a comment)
+            if line.startswith("#"):
+                pass
+            # If newgame is equal to true, count the lines until it's
+            if newgame == True:
+                # If the ln variable is 4 (the number of line a game
+                # should be in the gameslist), add one to the number of
+                # games.
+                if ln == 4:
+                    number_of_games += 1
+                if line.startswith("#"):
+                    pass
+                else:
+                    ln += 1
+
+            else: 
+                pass
+
+            # If the line starts with "NEW GAME:"
+            if line.startswith("NEW GAME:"):
+                newgame = True
+                ln = 0
 
 
 
@@ -18,4 +45,3 @@ getNumberOfGames()
 print("No Command Specified")
 print("Or, if you want to know the commands, then sure! here:\n")
 print("There are currently no commands xD")
-input()
